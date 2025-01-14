3331022ddacf5a452b4758e04c68d79d54a0449e
glitchisno1 <olamiposioladinni14@gmail.com>
2025-01-14 21:32:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ global.bankowner = "FADARE"
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.wlcm = true
-global.autoblockmorroco = true //auto block 212 (true to on, false to off)
-global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
 global.xprefix = '.'
 
